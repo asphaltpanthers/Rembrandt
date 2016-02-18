f0e114e9988b261e9b384e02008246731e2cc2d9
diser <diser@asphaltpanthers.com>
2016-02-18 03:21:06 -0500

    Revert "Rollback nightwatch."
    This reverts commit b1c005069601ad42cf22efe349c8347393eb8c1f.

---
+++ Rembrandt/public/nightwatch/gulpfile.js
@@ -0,0 +1,37 @@
+﻿var gulp = require('gulp');
+var nightwatch = require('gulp-nightwatch');
+var jslint = require('gulp-jslint');
+
+gulp.task('test', ['lint'], function () {
+    "use strict";
+    return gulp.src('')
+        .pipe(nightwatch({
+            configFile: 'nightwatch.json'
+        }));
+});
+
+gulp.task('baseline', ['lint'], function () {
+    "use strict";
+    return gulp.src('')
+        .pipe(nightwatch({
+            configFile: 'nightwatch.json',
+            cliArgs: [ '--env baseline' ]
+        }));
+});
+
+gulp.task('lint', function () {
+    "use strict";
+    return gulp.src('**/*.js')
+        .pipe(jslint({
+            node: true,
+            evil: true,
+            nomen: true,
+            global: [],
+            predef: [],
+            reporter: 'default',
+            errorsOnly: false
+        }))
+        .on('error', function (error) {
+            console.error(String(error));
+        });
+});
