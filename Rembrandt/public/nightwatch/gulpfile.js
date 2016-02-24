c5f256039ea20a469d6c347182e1e29546e8d70c
diser <diser@asphaltpanthers.com>
2016-02-24 06:13:38 -0500

    Add image comparison to the end of the test.

--- Rembrandt/public/nightwatch/gulpfile.js
+++ Rembrandt/public/nightwatch/gulpfile.js
@@ -1,8 +1,17 @@
 ﻿var gulp = require('gulp');
 var nightwatch = require('gulp-nightwatch');
 var jslint = require('gulp-jslint');
+var resemble = require('node-resemble-js');
+var fs = require('fs');
+var path = require('path');
+var sequence = require('run-sequence');
 
-gulp.task('test', ['lint'], function () {
+gulp.task('test', function (callback) {
+    "use strict";
+    return sequence('lint', 'takeScreenshots', 'compare', callback);
+});
+
+gulp.task('takeScreenshots', function () {
     "use strict";
     return gulp.src('')
         .pipe(nightwatch({
@@ -17,6 +26,42 @@ gulp.task('baseline', ['lint'], function () {
             configFile: 'nightwatch.json',
             cliArgs: [ '--env baseline' ]
         }));
+});
+
+gulp.task('compare', function () {
+    "use strict";
+    fs.readdir('./screenshots', function (err, files) {
+        if (err) {
+            console.error(err);
+        }
+        files.forEach(function (file) {
+            if (path.extname(file) === '.png') {
+                fs.readdir('./screenshots/baseline', function (err, baselineFiles) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    if (baselineFiles.indexOf(file) <= -1) {
+                        return console.error('Baseline does not exist!');
+                    }
+                    fs.readFile('./screenshots/' + file, function (err, data) {
+                        if (err) {
+                            console.error(err);
+                        }
+                        fs.readFile('./screenshots/baseline/' + file, function (err, baselineData) {
+                            if (err) {
+                                console.error(err);
+                            }
+                            resemble(data).compareTo(baselineData).onComplete(function (comparisonData) {
+                                if (comparisonData.misMatchPercentage > 0) {
+                                    console.error('The screenshot \'' + file + '\' does not match the baseline!');
+                                }
+                            });
+                        });
+                    });
+                });
+            }
+        });
+    });
 });
 
 gulp.task('lint', function () {
