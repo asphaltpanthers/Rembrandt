4daa2761e68e21a7dad1406f4fa2e5be07c4a938
diser <diser@asphaltpanthers.com>
2016-06-06 22:04:19 -0400

    Start deleting old project.

--- Rembrandt/public/nightwatch/commands/rembrandtScreenshot.js
+++
@@ -1,7 +0,0 @@
-﻿var index = 1;
-
-exports.command = function () {
-    "use strict";
-    this.saveScreenshot(this.globals.screenshotPath + '/screenshot' + index + '.' + this.globals.browserName + '.png');
-    index = index + 1;
-};
